cffdbe30df3813c0048f8d638d88f41fe246fcac
Valdeci177 <159965356+Valdeci177@users.noreply.github.com>
2024-02-14 20:19:43 -0300

    Add files via upload

---
+++ Aula3-ex1.py
@@ -0,0 +1,42 @@
+alunos = {}
+
+def adicionar_aluno():
+    nome = input("Digite o nome do aluno: ")
+    matricula = input("Digite o número de matrícula do aluno: ")
+    alunos[matricula] = nome
+    print("Aluno adicionado com sucesso!")
+
+def remover_aluno():
+    matricula = input("Digite o número de matrícula do aluno a ser removido: ")
+    if matricula in alunos:
+        del alunos[matricula]
+        print("Aluno removido com sucesso!")
+    else:
+        print("Aluno não encontrado.")
+
+def visualizar_alunos():
+    if alunos:
+        print("Lista de alunos:")
+        for matricula, nome in alunos.items():
+            print(f"Número de matrícula: {matricula}, Nome: {nome}")
+    else:
+        print("Nenhum aluno registrado.")
+
+while True:
+    print("\n1. Adicionar aluno")
+    print("2. Remover aluno")
+    print("3. Visualizar todos os alunos")
+    print("4. Sair")
+    opcao = input("Escolha uma opção: ")
+
+    if opcao == '1':
+        adicionar_aluno()
+    elif opcao == '2':
+        remover_aluno()
+    elif opcao == '3':
+        visualizar_alunos()
+    elif opcao == '4':
+        print("Encerrando o programa...")
+        break
+    else:
+        print("Opção inválida. Por favor, escolha uma opção válida.")
